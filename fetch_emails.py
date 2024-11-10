b012908b72b191f688e717e31a2dd63c3fccb503
navomi09 <navomisr9@gmail.com>
2024-11-10 15:36:14 +0530

    Initial commit

--- fetch_emails.py
+++ fetch_emails.py
@@ -4,24 +4,22 @@ import imaplib
 import email
 from email.header import decode_header
 
-# Load environment variables
 load_dotenv()
 
 def fetch_emails():
     email_user = os.getenv('EMAIL_USER')
     email_pass = os.getenv('EMAIL_PASS')
     
-    # Connect to email server
+
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
     mail.login(email_user, email_pass)
     mail.select('inbox')
 
-    # Fetch all emails (you can also filter for unseen emails)
-    _, messages = mail.search(None, 'UNSEEN')  # Fetch only unread emails
+    _, messages = mail.search(None, 'UNSEEN')  
     email_ids = messages[0].split()
 
     emails = []
-    for e_id in email_ids[-5:]:  # Fetch last 5 emails (change if needed)
+    for e_id in email_ids[-5:]:  
         _, data = mail.fetch(e_id, '(RFC822)')
         raw_email = data[0][1]
         msg = email.message_from_bytes(raw_email)
@@ -31,7 +29,7 @@ def fetch_emails():
         if isinstance(subject, bytes):
             subject = subject.decode(encoding if encoding else 'utf-8')
 
-        # Get email body (plaintext)
+      
         body = ""
         if msg.is_multipart():
             for part in msg.walk():
@@ -40,7 +38,6 @@ def fetch_emails():
         else:
             body = msg.get_payload(decode=True).decode("utf-8")
 
-        emails.append((subject, body))  # Store subject and body
-
+        emails.append((subject, body))  
     mail.logout()
     return emails
